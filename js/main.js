cf3fea4e238fbc604b90bdc336f6c0d726584215
twinkfrag <mail@twinkfrag.net>
2013-03-27 17:55:08 +0900

    color

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var li = "elements/0.jpg elements/1.jpg elements/2.jpg elements/3.jpg elements/4.jpg elements/5.jpg elements/6.jpg elements/7.jpg elements/8.jpg elements/9.jpg".split(" ")
+var li = "elements/0.jpg elements/1.jpg elements/2.jpg elements/3.jpg elements/4.jpg elements/5.jpg elements/6.jpg elements/7.jpg elements/8.jpg elements/9.jpg".split(" ");
+//var col = "0004B3 24B300 5AB300 8fb300 b3a100 b36b00 b33600 b30000 b30036 b3006b".split(" ");
+var col = "2f415e 385e2f 475e2f 545e2f 5e592f 5e4b2f 5e3d2f 5e2f2f 5e2f3d 5e2f4b".split(" ");
 
 $(document).ready(function(){
 	var rnd = Math.floor(li.length * Math.random());
@@ -13,9 +15,9 @@ $(document).ready(function(){
 				"background-position-y": height + "px"
 			}, 3000);
 	}
-  $(".club").css("background-image", function(){
+  $(".club").css("background-color", function(){
     i++;
-    return "url(" + li[i-1] + ")";
+    return "#" + col[i-1];
   });
   $(".club").mouseover(function(){
       $(this).fadeTo("fast", 1)
