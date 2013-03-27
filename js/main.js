356be8c8dc53021eff0ade46e134cbd74780372d
twinkfrag <mail@twinkfrag.net>
2013-03-27 13:37:50 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var li = "elements/0.jpg elements/1.jpg elements/2.jpg elements/3.jpg elements/4.jpg elements/5.jpg elements/6.jpg elements/7.jpg elements/8.jpg elements/9.jpg".split(" ")
+
+$(document).ready(function(){
+	var rnd = Math.floor(li.length * Math.random());
+	$("#header")
+		.css("opacity", "0")
+		.css("background-image", "url(" + li[rnd] + ")")
+		.animate({
+			opacity: 1
+		}, 1000);
+	
+	var i = 0;
+		$(".club").css("background-image", function(){
+			i++;
+			return "url(" + li[i-1] + ")";
+		});
+});
